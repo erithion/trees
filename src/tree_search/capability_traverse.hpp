03b629994c2473e11418aa15c14ed637fb1521bd
Artyom Bondartsov <erithion@gmail.com>
2020-02-12 00:17:26 +0200

    + inorder iterator + removed deprecated std::iterator

--- src/tree_search/capability_traverse.hpp
+++ src/tree_search/capability_traverse.hpp
@@ -2,21 +2,25 @@
 
 #include "type_traverse.hpp"
 
-#include <iterator>
 #include <stack>
 
 namespace tree_search {
 
-    template <typename T, typename Node, typename TraverseTag>
+    template <typename Node, typename TraverseTag>
     struct traverse_iterator;
 
-    template <typename T, typename Node>
-    struct traverse_iterator<T, Node, tag_preorder>
-        : public std::iterator<std::forward_iterator_tag, T> {
+    template <typename Node>
+    struct traverse_iterator<Node, tag_preorder> {
+
+        using iterator_category = std::forward_iterator_tag;
+        using value_type = typename Node::value_type;
+        using difference_type = std::ptrdiff_t;
+        using pointer = const value_type*;
+        using reference = const value_type&;
 
         traverse_iterator() = default; // end iterator
         explicit traverse_iterator(Node* t) { this->s_.push(t); }
-        traverse_iterator& operator++() { this->go(); return *this; }
+        traverse_iterator& operator++() { this->step(); return *this; }
         traverse_iterator operator++(int) { traverse_iterator retval = *this; ++(*this); return retval; }
         bool operator==(traverse_iterator other) const { return this->s_ == other.s_; }
         bool operator!=(traverse_iterator other) const { return !(*this == other); }
@@ -25,7 +29,7 @@ namespace tree_search {
     private:
         std::stack<Node*> s_ = {};
 
-        void go() {
+        void step() {
             if (this->s_.empty()) return;
             auto last = this->s_.top();
             this->s_.pop();
@@ -34,13 +38,18 @@ namespace tree_search {
         }
     };
 
-    template <typename T, typename Node>
-    struct traverse_iterator<T, Node, tag_postorder>
-        : public std::iterator<std::forward_iterator_tag, T> {
+    template <typename Node>
+    struct traverse_iterator<Node, tag_postorder> {
+
+        using iterator_category = std::forward_iterator_tag;
+        using value_type = typename Node::value_type;
+        using difference_type = std::ptrdiff_t;
+        using pointer = const value_type*;
+        using reference = const value_type&;
 
         traverse_iterator() = default; // end iterator
-        explicit traverse_iterator(Node* t) { this->s_.emplace(cont{t}); this->go(); }
-        traverse_iterator& operator++() { this->go(); return *this; }
+        explicit traverse_iterator(Node* t) { this->s_.emplace(cont{t}); this->step(); }
+        traverse_iterator& operator++() { this->step(); return *this; }
         traverse_iterator operator++(int) { traverse_iterator retval = *this; ++(*this); return retval; }
         bool operator==(traverse_iterator other) const { return this->s_ == other.s_; }
         bool operator!=(traverse_iterator other) const { return !(*this == other); }
@@ -49,13 +58,12 @@ namespace tree_search {
     private:
         struct cont {
             Node* ptr_ = nullptr;
-            volatile bool handled_ = false; // true if childrens were handled
+            volatile bool handled_ = false; // true if children were handled
             bool operator ==(const cont& other) const { return other.handled_ == this->handled_ && other.ptr_ == this->ptr_; }
         };
-
         std::stack<cont> s_ = {};
 
-        void go() {
+        void step() {
             if (this->s_.empty()) return;
 
             if (this->s_.top().handled_) this->s_.pop();
@@ -68,23 +76,42 @@ namespace tree_search {
         }
     };
 
-    template <typename T, typename Node>
-    struct traverse_iterator<T, Node, tag_inorder>
-        : public std::iterator<std::forward_iterator_tag, T> {
+    template <typename Node>
+    struct traverse_iterator<Node, tag_inorder> {
+
+        using iterator_category = std::forward_iterator_tag;
+        using value_type = typename Node::value_type;
+        using difference_type = std::ptrdiff_t;
+        using pointer = const value_type*;
+        using reference = const value_type&;
 
         traverse_iterator() = default; // end iterator
-        explicit traverse_iterator(Node* t) { this->s_.push(t); }
-        traverse_iterator& operator++() { this->go(); return *this; }
+        explicit traverse_iterator(Node* t) { this->s_.emplace(cont{ t }); this->step(); }
+        traverse_iterator& operator++() { this->step(); return *this; }
         traverse_iterator operator++(int) { traverse_iterator retval = *this; ++(*this); return retval; }
         bool operator==(traverse_iterator other) const { return this->s_ == other.s_; }
         bool operator!=(traverse_iterator other) const { return !(*this == other); }
-        reference operator*() const { return this->s_.top()->value_; }
+        reference operator*() const { return this->s_.top().ptr_->value_; }
 
     private:
-        std::stack<Node*> s_ = {};
+        struct cont {
+            Node* ptr_ = nullptr;
+            bool exit_ = false;
+            bool operator ==(const cont& other) const { return other.exit_ == this->exit_ && other.ptr_ == this->ptr_; }
+        };
+        std::stack<cont> s_ = {};
 
-        void go() {
-            throw;
+        void step() {
+            if (this->s_.empty()) return;
+
+            if (this->s_.top().exit_) this->s_.pop();
+            if (!this->s_.empty() && !this->s_.top().exit_) 
+                for (auto p = this->s_.top().ptr_; p; p = p->left_.get()) {
+                    this->s_.pop();
+                    if (p->right_) this->s_.emplace(cont{ p->right_.get() });
+                    this->s_.emplace(cont{ p, true });
+                    if (p->left_) this->s_.emplace(cont{ p->left_.get(), true });
+                }
         }
     };
   
@@ -98,10 +125,7 @@ namespace tree_search {
 
     template <typename Tag, typename Tree, std::enable_if_t<std::is_base_of_v<capability_traverse, Tree>, int> = 0>
     auto traverse(Tag&& tag, const Tree& tree) {
-        using node_type = typename Tree::node_type;
-        using value_type = typename Tree::value_type;
-        using iterator_type = tree_search::traverse_iterator<value_type, node_type, Tag>;
-
+        using iterator_type = tree_search::traverse_iterator<typename Tree::node_type, Tag>;
         return iterators<iterator_type>{ iterator_type(tree.root_.get()) };
     }
 }

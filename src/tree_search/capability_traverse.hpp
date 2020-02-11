e7ed1844f58173866a9867320968a5e85a98fb48
Artyom Bondartsov <erithion@gmail.com>
2020-02-11 03:54:10 +0200

    + ending iterator

--- src/tree_search/capability_traverse.hpp
+++ src/tree_search/capability_traverse.hpp
@@ -2,30 +2,28 @@
 
 #include "type_traverse.hpp"
 
-#include <functional>
-#include <type_traits>
-#include <memory>
 #include <iterator>
 #include <stack>
 
 namespace tree_search {
 
     template <typename T, typename Node, typename TraverseTag>
-    struct iterator_;
+    struct traverse_iterator;
 
     template <typename T, typename Node>
-    struct iterator_<T, Node, tag_preorder>
+    struct traverse_iterator<T, Node, tag_preorder>
         : public std::iterator<std::forward_iterator_tag, T> {
 
-        explicit  iterator_(Node* t) : s_() { this->s_.push(t); }
-        iterator_& operator++() { this->go(); return *this; }
-        iterator_ operator++(int) { iterator_ retval = *this; ++(*this); return retval; }
-        bool operator==(iterator_ other) const { return this->s_ == other.s_; }
-        bool operator!=(iterator_ other) const { return !(*this == other); }
+        traverse_iterator() = default; // end iterator
+        explicit traverse_iterator(Node* t) { this->s_.push(t); }
+        traverse_iterator& operator++() { this->go(); return *this; }
+        traverse_iterator operator++(int) { traverse_iterator retval = *this; ++(*this); return retval; }
+        bool operator==(traverse_iterator other) const { return this->s_ == other.s_; }
+        bool operator!=(traverse_iterator other) const { return !(*this == other); }
         reference operator*() const { return this->s_.top()->value_; }
 
     private:
-        std::stack<Node*> s_;
+        std::stack<Node*> s_ = {};
 
         void go() {
             if (this->s_.empty()) return;
@@ -35,113 +33,75 @@ namespace tree_search {
             if (last->left_) this->s_.push(last->left_.get());
         }
     };
+
     template <typename T, typename Node>
-    struct iterator_<T, Node, tag_postorder>
+    struct traverse_iterator<T, Node, tag_postorder>
         : public std::iterator<std::forward_iterator_tag, T> {
 
-        explicit  iterator_(Node* t) { this->s_.push(std::make_pair(t, false)); this->go(); }
-        iterator_& operator++() { this->go(); return *this; }
-        iterator_ operator++(int) { iterator_ retval = *this; ++(*this); return retval; }
-        bool operator==(iterator_ other) const { return this->s_ == other.s_; }
-        bool operator!=(iterator_ other) const { return !(*this == other); }
-        reference operator*() const { return this->s_.top().first->value_; }
+        traverse_iterator() = default; // end iterator
+        explicit traverse_iterator(Node* t) { this->s_.emplace(cont{t}); this->go(); }
+        traverse_iterator& operator++() { this->go(); return *this; }
+        traverse_iterator operator++(int) { traverse_iterator retval = *this; ++(*this); return retval; }
+        bool operator==(traverse_iterator other) const { return this->s_ == other.s_; }
+        bool operator!=(traverse_iterator other) const { return !(*this == other); }
+        reference operator*() const { return this->s_.top().ptr_->value_; }
 
     private:
-        std::stack<std::pair<Node*, volatile bool>> s_ = {};
+        struct cont {
+            Node* ptr_ = nullptr;
+            volatile bool handled_ = false; // true if childrens were handled
+            bool operator ==(const cont& other) const { return other.handled_ == this->handled_ && other.ptr_ == this->ptr_; }
+        };
 
-        void put_children() {
-            auto& top = this->s_.top();
-            if (!top.second) {
-                top.second = true;
-                if (top.first->right_) this->s_.emplace(std::make_pair(top.first->right_.get(), false));
-                if (top.first->left_) this->s_.emplace(std::make_pair(top.first->left_.get(), false));
-            }
-        }
+        std::stack<cont> s_ = {};
+
         void go() {
             if (this->s_.empty()) return;
 
-            if (this->s_.top().second) this->s_.pop();
-            while (!this->s_.top().second) this->put_children();
+            if (this->s_.top().handled_) this->s_.pop();
+            while (!this->s_.empty() && !this->s_.top().handled_) { // push children
+                auto& top = this->s_.top();
+                top.handled_ = true;
+                if (top.ptr_->right_) this->s_.emplace(cont{ top.ptr_->right_.get() });
+                if (top.ptr_->left_) this->s_.emplace(cont{ top.ptr_->left_.get() });
+            }
         }
     };
 
-    //template <typename T, typename Tree>
-    //struct iterator<T, Tree, tag_inorder>
-    //    : public std::iterator<T, std::forward_iterator_tag> {
+    template <typename T, typename Node>
+    struct traverse_iterator<T, Node, tag_inorder>
+        : public std::iterator<std::forward_iterator_tag, T> {
 
-    //    explicit  iterator(const Tree* t)
-    //        : s_(t), f_(false) {}
+        traverse_iterator() = default; // end iterator
+        explicit traverse_iterator(Node* t) { this->s_.push(t); }
+        traverse_iterator& operator++() { this->go(); return *this; }
+        traverse_iterator operator++(int) { traverse_iterator retval = *this; ++(*this); return retval; }
+        bool operator==(traverse_iterator other) const { return this->s_ == other.s_; }
+        bool operator!=(traverse_iterator other) const { return !(*this == other); }
+        reference operator*() const { return this->s_.top()->value_; }
 
-    //    iterator& operator++() { num = TO >= FROM ? num + 1 : num - 1; return *this; }
-    //    iterator operator++(int) { iterator retval = *this; ++(*this); return retval; }
-    //    bool operator==(iterator other) const { return num == other.num; }
-    //    bool operator!=(iterator other) const { return !(*this == other); }
-    //    reference operator*() const { return this->s_.top()->value_; }
+    private:
+        std::stack<Node*> s_ = {};
 
-    //private:
-    //    std::stack<const Tree*> s_;
-    //    bool f_;
-
-    //    void go() {
-    //        while (!this->f_ && this->s_.top()->left_) {
-    //            this->s_.push(this->s_.top()->left_);
-    //            return;
-    //        }
-    //        if (!this->f_) {
-    //            auto last = this->s_.top();
-    //            this->s_.pop();
-    //            if (last->right_) {
-    //                this->s_.push(last->right_);
-    //                this->go();
-    //            } else 
-    //                this->f_ = true;
-    //            return;
-    //        }
-    //        this->f_ = false;
-    //        if (this->s_.top()->right) {
-    //            this->s_.push(this->s_.top()->right);
-    //            this->go();
-    //        }
-    //    }
-    //};
+        void go() {
+            throw;
+        }
+    };
   
+    template <typename It>
+    struct iterators {
+        It begin;
+        It end = {};
+    };
 
     struct capability_traverse {};
 
-    namespace aux {
-
-        template <typename Tree, typename Tag>
-        auto traverse(const std::unique_ptr<Tree>& tree, Tag&&) {
-            return tree_search::iterator_<typename Tree::value_type, Tree, Tag>(tree.get());
-        }
-        //template <typename Tree>
-        //void traverse(const std::unique_ptr<Tree>& tree, const std::function<void(const typename Tree::value_type&)>& fn, tag_inorder&&) {
-        //    if (!tree) return;
-        //    traverse(tree->left_, fn, tag_inorder{});
-        //    if (fn) fn(tree->value_);
-        //    traverse(tree->right_, fn, tag_inorder{});
-        //}
-
-        //template <typename Tree>
-        //void traverse(const std::unique_ptr<Tree>& tree, const std::function<void(const typename Tree::value_type)>& fn, tag_preorder&&) {
-        //    if (!tree) return;
-        //    if (fn) fn(tree->value_);
-        //    traverse(tree->left_, fn, tag_preorder{});
-        //    traverse(tree->right_, fn, tag_preorder{});
-        //}
-
-        //template <typename Tree>
-        //void traverse(const std::unique_ptr<Tree>& tree, const std::function<void(const typename Tree::value_type&)>& fn, tag_postorder&&) {
-        //    if (!tree) return;
-        //    traverse(tree->left_, fn, tag_postorder{});
-        //    traverse(tree->right_, fn, tag_postorder{});
-        //    if (fn) fn(tree->value_);
-        //}
-    }
-
-    // TODO: implement iterators for traversing
     template <typename Tag, typename Tree, std::enable_if_t<std::is_base_of_v<capability_traverse, Tree>, int> = 0>
     auto traverse(Tag&& tag, const Tree& tree) {
-        return aux::traverse(tree.root_, std::forward<Tag>(tag));
+        using node_type = typename Tree::node_type;
+        using value_type = typename Tree::value_type;
+        using iterator_type = tree_search::traverse_iterator<value_type, node_type, Tag>;
+
+        return iterators<iterator_type>{ iterator_type(tree.root_.get()) };
     }
 }

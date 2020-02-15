691963a7715cc389e18a7e30c1f4cf7b34a03809
Artyom Bondartsov <erithion@gmail.com>
2020-02-15 22:15:31 +0200

    inorder interval search iterator

--- config.bat
+++ config.bat
@@ -1,5 +1,5 @@
 mkdir build
 cd build
 
-:: cmake -G "Visual Studio 15 2017" ..
-cmake ..
+cmake -G "Visual Studio 15 2017" ..
+:: cmake ..
--- src/tree_search/capability_search_interval.hpp
+++ src/tree_search/capability_search_interval.hpp
@@ -14,42 +14,52 @@ namespace tree_search {
         template <typename Node>
         struct preorder_search {
             using cont = cont<Node>;
+            using value_type = typename Node::value_type;
             void step() {
-                if (this->s_.empty()) return;
+                if (this->stack_.empty()) return;
 
                 do {
-                    auto last = this->s_.top();
-                    this->s_.pop();
+                    auto last = this->stack_.top();
+                    this->stack_.pop();
                     if (last.visited_) continue;
-                    if (last.ptr_->right_ && last.ptr_->value_.first <= this->interval_.second) this->s_.emplace(cont{ last.ptr_->right_.get() });
-                    if (last.ptr_->left_ && last.ptr_->left_->augment_.max_ >= this->interval_.first) this->s_.emplace(cont{ last.ptr_->left_.get() });
+                    if (last.ptr_->right_ && last.ptr_->value_.first <= this->bounds_.second)
+                        this->stack_.emplace(cont{ last.ptr_->right_.get() });
+                    if (last.ptr_->left_ && last.ptr_->left_->augment_.max_ >= this->bounds_.first)
+                        this->stack_.emplace(cont{ last.ptr_->left_.get() });
 
-                    if (this->interval_.first <= last.ptr_->value_.second && this->interval_.second >= last.ptr_->value_.first) {
+                    if (this->bounds_.first <= last.ptr_->value_.second && this->bounds_.second >= last.ptr_->value_.first) {
                         last.visited_ = true;
-                        this->s_.emplace(last);
+                        this->stack_.emplace(last);
                     }
-                } while (!this->s_.empty() && !this->s_.top().visited_);
+                } while (!this->stack_.empty() && !this->stack_.top().visited_);
             }
-            std::stack<cont> s_ = {};
-            typename Node::value_type interval_ = {};
+            std::stack<cont> stack_ = {};
+            value_type       bounds_ = {};
         };
 
         template <typename Node>
         struct inorder_search {
             using cont = cont<Node>;
+            using value_type = typename Node::value_type;
             void step() {
-                if (this->s_.empty()) return;
-
-                if (this->s_.top().visited_) this->s_.pop();
-                if (!this->s_.empty() && !this->s_.top().visited_)
-                    for (auto p = this->s_.top().ptr_; p; p = p->left_.get()) {
-                        this->s_.pop();
-                        if (p->right_) this->s_.emplace(cont{ p->right_.get() });
-                        this->s_.emplace(cont{ p, true });
-                        if (p->left_) this->s_.emplace(cont{ p->left_.get(), true });
-                    }
+                if (!this->stack_.empty() && this->stack_.top().visited_) this->stack_.pop();
+                // nodes on the stack are always rearranged in order (from the top): left; top; right.
+                // this has an effect as if we were always moving first to the left, then top, then right.
+                // we skip the nodes beyond the interval's bound. 
+                // upon the loop exit we always have the next lowest node within the interval bounds.
+                while (!this->stack_.empty() && !this->stack_.top().visited_) {
+                    auto p = this->stack_.top().ptr_;
+                    this->stack_.pop();
+                    if (p->right_ && p->right_->value_.first <= this->bounds_.second)
+                        this->stack_.emplace(cont{ p->right_.get() });
+                    if (this->bounds_.first <= p->value_.second && this->bounds_.second >= p->value_.first)
+                        this->stack_.emplace(cont{ p, true });
+                    if (p->left_ && p->left_->augment_.max_ >= this->bounds_.first)
+                        this->stack_.emplace(cont{ p->left_.get() });
+                }
             }
-            std::stack<cont> s_ = {};
+            std::stack<cont> stack_ = {};
+            value_type       bounds_ = {};
         };
 
         template <typename Node>
@@ -92,12 +102,12 @@ namespace tree_search {
         using reference = const value_type&;
 
         explicit search_iterator_interval(const value_type& interval) {
-            this->interval_ = interval;
+            this->bounds_ = interval;
         }
 
         search_iterator_interval(Node* root, const value_type& interval) {
-            this->s_.emplace(cont{ root });
-            this->interval_ = interval;
+            this->stack_.emplace(cont{ root });
+            this->bounds_ = interval;
             this->step();
         }
         search_iterator_interval& operator++() {
@@ -110,13 +120,13 @@ namespace tree_search {
             return retval;
         }
         bool operator==(const search_iterator_interval& other) const {
-            return this->s_ == other.s_ && this->interval_ == other.interval_;
+            return this->stack_ == other.stack_ && this->bounds_ == other.bounds_;
         }
         bool operator!=(const search_iterator_interval& other) const {
             return !(*this == other);
         }
         reference operator*() const {
-            return this->s_.top().ptr_->value_;
+            return this->stack_.top().ptr_->value_;
         }
     };
 
@@ -127,28 +137,6 @@ namespace tree_search {
 
         template <typename Node>
         using callback = std::function<void(const typename Node::value_type&)>;
-
-        template <typename T, typename Node>
-        void search(const std::unique_ptr<Node>& tree, const std::pair<T, T>& interval, const callback<Node>& fn, tag_inorder&&) {
-            if (!tree) return;
-            if (tree->left_ && tree->left_->augment_.max_ >= interval.first)
-                search(tree->left_, interval, fn, tag_inorder{});
-            if (interval.first <= tree->value_.second && !(interval.second < tree->value_.first))
-                if (fn) fn(tree->value_);
-            if (tree->value_.first <= interval.second)
-                search(tree->right_, interval, fn, tag_inorder{});
-        }
-
-        template <typename T, typename Node>
-        void search(const std::unique_ptr<Node>& tree, const std::pair<T, T>& interval, const callback<Node>& fn, tag_preorder&&) {
-            if (!tree) return;
-            if (interval.first <= tree->value_.second && !(interval.second < tree->value_.first))
-                if (fn) fn(tree->value_);
-            if (tree->left_ && tree->left_->augment_.max_ >= interval.first)
-                search(tree->left_, interval, fn, tag_inorder{});
-            if (tree->value_.first <= interval.second)
-                search(tree->right_, interval, fn, tag_inorder{});
-        }
 
         template <typename T, typename Node>
         void search(const std::unique_ptr<Node>& tree, const std::pair<T, T>& interval, const callback<Node>& fn, tag_postorder&&) {

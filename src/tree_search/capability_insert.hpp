886e50521644a1852776f209f14b56057c245f1c
Artyom Bondartsov <erithion@gmail.com>
2020-03-11 00:52:30 +0200

    revert insert_or_assign

--- src/tree_search/capability_insert.hpp
+++ src/tree_search/capability_insert.hpp
@@ -13,46 +13,41 @@ namespace tree_search {
         template <typename Node>
         void fixup(std::unique_ptr<Node>& t, capability_insert) { } // for the cases when a tree has some invariants to restore
 
-        struct tag_insert {};
-        struct tag_assign {};
-
-        template <typename T, typename Node>
-        inline bool assign(std::unique_ptr<Node>& tree, tag_insert, T&& v) { return false; }
-
-        template <typename T, typename Node>
-        inline bool assign(std::unique_ptr<Node>& tree, tag_assign, T&& v) { 
-            tree->value_ = std::forward<T>(v);
-            return true;
+        template <typename T, typename Node, typename CapabilityTag, typename ... Aug>
+        void insert_or_assign(std::unique_ptr<Node>& tree, CapabilityTag ctag, T&& v, Aug&& ... pack) { // universal reference
+            if (!tree) tree = std::make_unique<node_type_t<Node>>(std::forward<T>(v), std::forward<Aug>(pack)...);
+            else if (v < tree->value_) insert(tree->left_, ctag, std::forward<T>(v), std::forward<Aug>(pack)...);
+            else if (v == tree->value_) tree->value_ = std::forward<T>(v);
+            else if (!(v < tree->value_)) insert(tree->right_, ctag, std::forward<T>(v), std::forward<Aug>(pack)...);
+            fixup(tree, ctag);
         }
 
-        template <typename T, typename Node, typename CapabilityTag, typename InsertTag, typename ... Aug>
-        void insert(std::unique_ptr<Node>& tree, CapabilityTag ctag, InsertTag itag, T&& v, Aug&& ... pack) { // universal reference
+        template <typename T, typename Node, typename CapabilityTag, typename ... Aug>
+        void insert(std::unique_ptr<Node>& tree, CapabilityTag ctag, T&& v, Aug&& ... pack) { // universal reference
             if (!tree) tree = std::make_unique<node_type_t<Node>>(std::forward<T>(v), std::forward<Aug>(pack)...);
-            else if (v < tree->value_) insert(tree->left_, ctag, itag, std::forward<T>(v), std::forward<Aug>(pack)...);
-            else if (v == tree->value_) { // found. now assign if it is assign_or_insert
-                if (!assign(tree, itag, std::forward<T>(v))) return;
-            }
-            else if (!(v < tree->value_)) insert(tree->right_, ctag, itag, std::forward<T>(v), std::forward<Aug>(pack)...);
+            else if (v < tree->value_) insert(tree->left_, ctag, std::forward<T>(v), std::forward<Aug>(pack)...);
+            else if (v == tree->value_) return;
+            else if (!(v < tree->value_)) insert(tree->right_, ctag, std::forward<T>(v), std::forward<Aug>(pack)...);
             fixup(tree, ctag);
         }
     }
 
     template <typename T, typename Tree, std::enable_if_t<std::is_base_of_v<capability_insert, Tree>, int> = 0>
     void insert(Tree& tree, T&& v) { // universal reference
-        aux::insert(aux::access(tree), capability_insert{}, aux::tag_insert{}, std::forward<T>(v));
+        aux::insert(aux::access(tree), capability_insert{}, std::forward<T>(v));
     }
     template <typename T, typename Tree, std::enable_if_t<std::is_base_of_v<capability_insert, Tree>, int> = 0>
     void insert_or_assign(Tree& tree, T&& v) { // universal reference
-        aux::insert(aux::access(tree), capability_insert{}, aux::tag_assign{}, std::forward<T>(v));
+        aux::insert_or_assign(aux::access(tree), capability_insert{}, std::forward<T>(v));
     }
 
     template <typename T, typename Tree, std::enable_if_t<std::is_base_of_v<capability_insert, Tree>, int> = 0>
     void insert(Tree& tree, std::initializer_list<T> ls) {
-        for (auto&& v : ls) aux::insert(aux::access(tree), capability_insert{}, aux::tag_insert{}, std::move(v));
+        for (auto&& v : ls) aux::insert(aux::access(tree), capability_insert{}, std::move(v));
     }
     template <typename T, typename Tree, std::enable_if_t<std::is_base_of_v<capability_insert, Tree>, int> = 0>
     void insert_or_assign(Tree& tree, std::initializer_list<T> ls) {
-        for (auto&& v : ls) aux::insert(aux::access(tree), capability_insert{}, aux::tag_assign{}, std::move(v));
+        for (auto&& v : ls) aux::insert_or_assign(aux::access(tree), capability_insert{}, std::move(v));
     }
 
     template <typename It, typename Tree, std::enable_if_t<std::is_base_of_v<capability_insert, Tree>, int> = 0>
